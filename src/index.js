2d36e833b4456504c015764992a11de14e591bc1
Julie <julienne.bay@gmail.com>
2020-05-13 18:30:34 -0400

    when the form is submitted, it 'loads' for 2 seconds (simulates http request) and then shows a thank you message.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,13 @@ const INTERESTS = [
 ];
 
 class Form extends React.Component {
-  state = { submitError: false, email: "" };
+  state = {
+    submitted: false,
+    submitError: false,
+    showThanks: false,
+    email: "",
+    interest: ""
+  };
 
   submitForm = () => {
     if (
@@ -19,13 +25,28 @@ class Form extends React.Component {
       !this.state.email.match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g)
     ) {
       this.setState({ submitError: true });
+    } else {
+      console.log(
+        "email address: ",
+        this.state.email,
+        "interest: ",
+        this.state.interest
+      );
+      this.setState({ submitted: true });
+      setTimeout(() => {
+        this.setState({ showThanks: true });
+      }, 2000);
     }
   };
 
   updateEmail = event => {
     const email = event.target.value;
-    console.log(this.state);
     this.setState({ email: email });
+  };
+
+  updateInterest = event => {
+    const interest = event.target.value;
+    this.setState({ interest: interest });
   };
 
   render() {
@@ -33,43 +54,63 @@ class Form extends React.Component {
       <div className="main-pg">
         <h1 className="form-title">INTERNSHIP SIGNUP FORM</h1>
         <div className="line"></div>
-        <div className="intro">
-          <p>
-            Prepare for your career with a Project Management, Web-Development,
-            Graphic Design, or Digital Marketing Internship at Northern.
-          </p>
-        </div>
-        <div className="form">
-          <div className="input-fields">
-            <div>
-              {this.state.submitError ? (
-                <p className="error-msg">Check your email</p>
-              ) : (
-                ""
-              )}
-              <input
-                type="text"
-                className={`email-input ${
-                  this.state.submitError ? "error-input" : ""
-                }`}
-                placeholder="Your Email Address*"
-                onChange={this.updateEmail}
-              />
-            </div>
-            <select className="dropdown" required>
-              {INTERESTS.map((interest, i) => (
-                <option value={i !== 0 ? interest : ""}>{interest}</option>
-              ))}
-            </select>
+
+        {this.state.showThanks ? (
+          <div className="thanks-pg">
+            <h2 className="thanks-title">Thanks for your interest!</h2>
+            <p className="thanks-msg">
+              We will review your application and contact you for additional
+              information should your background and experience meet the
+              requirements of one of our openings.
+            </p>
           </div>
-          <button onClick={this.submitForm} className-="signup">
-            Sign Up Now ▸
-          </button>
-        </div>
+        ) : (
+          <>
+            <div className="intro">
+              <p>
+                Prepare for your career with a Project Management,
+                Web-Development, Graphic Design, or Digital Marketing Internship
+                at Northern.
+              </p>
+            </div>
+            <div className="form">
+              <div className="input-fields">
+                <div>
+                  {this.state.submitError ? (
+                    <p className="error-msg">Check your email</p>
+                  ) : (
+                    ""
+                  )}
+                  <input
+                    type="text"
+                    className={`email-input ${
+                      this.state.submitError ? "error-input" : ""
+                    }`}
+                    placeholder="Your Email Address*"
+                    onChange={this.updateEmail}
+                  />
+                </div>
+                <select
+                  className="dropdown"
+                  required
+                  onChange={this.updateInterest}
+                >
+                  {INTERESTS.map((interest, i) => (
+                    <option value={i !== 0 ? interest : ""}>{interest}</option>
+                  ))}
+                </select>
+              </div>
+              <button onClick={this.submitForm} className-="signup">
+                {this.state.submitted ? "Submitting..." : "Sign Up Now ▸"}
+              </button>
+            </div>
+          </>
+        )}
       </div>
     );
   }
 }
-//line 56 - the option without a given value (index 0) - different css value (see dropdown: invalid)
-//this only works because we put "required" on line 54, which means the value can't be empty
+
+//line 76 - the option without a given value (index 0) - different css value (see dropdown: invalid)
+//this only works because we put "required", which means the value can't be empty
 ReactDOM.render(<Form />, document.querySelector("#root"));
