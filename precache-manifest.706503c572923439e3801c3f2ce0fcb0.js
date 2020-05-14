ebf50661e68af85e9ef1e62459e9d6ba76e7e9d8
Julie <julienne.bay@gmail.com>
2020-05-14 15:06:18 -0400

    Updates

---
+++ precache-manifest.706503c572923439e3801c3f2ce0fcb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b780eb86f692ddc659b87897014e50c6",
+    "url": "/northern-application/index.html"
+  },
+  {
+    "revision": "f8eafd59ab9328d911bf",
+    "url": "/northern-application/static/css/main.dcbf116c.chunk.css"
+  },
+  {
+    "revision": "11e1d051cf20baa1fa3a",
+    "url": "/northern-application/static/js/2.b52e888a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/northern-application/static/js/2.b52e888a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8eafd59ab9328d911bf",
+    "url": "/northern-application/static/js/main.6178eee2.chunk.js"
+  },
+  {
+    "revision": "7e9750a975313aef8752",
+    "url": "/northern-application/static/js/runtime-main.ca00560b.js"
+  }
+]);
