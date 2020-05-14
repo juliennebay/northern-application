87c195152f2e7cc3d2e28c5004ea55976bee6389
Julie <julienne.bay@gmail.com>
2020-05-14 13:16:36 -0400

    cleaned up the code so that it's easier to read

--- src/index.js
+++ src/index.js
@@ -10,20 +10,34 @@ const INTERESTS = [
   "Sales"
 ];
 
+const ThanksPage = props => (
+  <div className="thanks-pg">
+    <h2 className="thanks-title">Thanks for your interest!</h2>
+    <p className="thanks-msg">
+      We will review your application and contact you for additional information
+      should your background and experience meet the requirements of one of our
+      openings.
+    </p>
+  </div>
+);
+
 class Form extends React.Component {
   state = {
-    submitted: false,
+    loading: false,
     submitError: false,
-    showThanks: false,
     email: "",
     interest: ""
   };
 
-  submitForm = () => {
-    if (
+  isEmailInvalid = () => {
+    return (
       this.state.email.length === 0 ||
       !this.state.email.match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g)
-    ) {
+    );
+  };
+
+  submitForm = () => {
+    if (this.isEmailInvalid()) {
       this.setState({ submitError: true });
     } else {
       console.log(
@@ -32,10 +46,11 @@ class Form extends React.Component {
         "interest: ",
         this.state.interest
       );
-      this.setState({ submitted: true });
+      this.setState({ loading: true });
       setTimeout(() => {
-        this.setState({ showThanks: true });
+        this.setState({ loading: false });
         localStorage.setItem("submitted", "true");
+        this.props.onSubmit();
       }, 2000);
     }
   };
@@ -52,67 +67,77 @@ class Form extends React.Component {
 
   render() {
     return (
+      <>
+        <div className="intro">
+          <p>
+            Prepare for your career with a Project Management, Web-Development,
+            Graphic Design, or Digital Marketing Internship at Northern.
+          </p>
+        </div>
+        <div className="form">
+          <div className="input-fields">
+            <div>
+              {this.state.submitError ? (
+                <p className="error-msg">Check your email</p>
+              ) : (
+                ""
+              )}
+              <input
+                type="text"
+                className={`email-input ${
+                  this.state.submitError ? "error-input" : ""
+                }`}
+                placeholder="Your Email Address*"
+                onChange={this.updateEmail}
+              />
+            </div>
+            <select
+              className="dropdown"
+              required
+              onChange={this.updateInterest}
+            >
+              {INTERESTS.map((interest, i) => (
+                <option value={i !== 0 ? interest : ""}>{interest}</option>
+              ))}
+            </select>
+          </div>
+          <button onClick={this.submitForm} className-="signup">
+            {this.state.loading ? "Submitting..." : "Sign Up Now ▸"}
+          </button>
+        </div>
+      </>
+    );
+  }
+}
+
+class Switch extends React.Component {
+  state = { showThanks: false };
+
+  setShowThanks = () => {
+    this.setState({ showThanks: true });
+  };
+
+  isFormSubmitted = () => {
+    return (
+      this.state.showThanks || localStorage.getItem("submitted") === "true"
+    );
+  };
+
+  render() {
+    return (
       <div className="main-pg">
         <h1 className="form-title">INTERNSHIP SIGNUP FORM</h1>
         <div className="line"></div>
-
-        {this.state.showThanks ||
-        localStorage.getItem("submitted") === "true" ? (
-          <div className="thanks-pg">
-            <h2 className="thanks-title">Thanks for your interest!</h2>
-            <p className="thanks-msg">
-              We will review your application and contact you for additional
-              information should your background and experience meet the
-              requirements of one of our openings.
-            </p>
-          </div>
+        {this.isFormSubmitted() ? (
+          <ThanksPage />
         ) : (
-          <>
-            <div className="intro">
-              <p>
-                Prepare for your career with a Project Management,
-                Web-Development, Graphic Design, or Digital Marketing Internship
-                at Northern.
-              </p>
-            </div>
-            <div className="form">
-              <div className="input-fields">
-                <div>
-                  {this.state.submitError ? (
-                    <p className="error-msg">Check your email</p>
-                  ) : (
-                    ""
-                  )}
-                  <input
-                    type="text"
-                    className={`email-input ${
-                      this.state.submitError ? "error-input" : ""
-                    }`}
-                    placeholder="Your Email Address*"
-                    onChange={this.updateEmail}
-                  />
-                </div>
-                <select
-                  className="dropdown"
-                  required
-                  onChange={this.updateInterest}
-                >
-                  {INTERESTS.map((interest, i) => (
-                    <option value={i !== 0 ? interest : ""}>{interest}</option>
-                  ))}
-                </select>
-              </div>
-              <button onClick={this.submitForm} className-="signup">
-                {this.state.submitted ? "Submitting..." : "Sign Up Now ▸"}
-              </button>
-            </div>
-          </>
+          <Form onSubmit={this.setShowThanks} />
         )}
       </div>
     );
   }
 }
 
-//line 76 - the option without a given value (index 0) - different css value (see dropdown: invalid)
+//note to myself: line 100 - the option without a given value (index 0) - different css value (see dropdown: invalid)
 //this only works because we put "required", which means the value can't be empty
-ReactDOM.render(<Form />, document.querySelector("#root"));
+ReactDOM.render(<Switch />, document.querySelector("#root"));
